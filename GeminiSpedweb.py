a016edf13ca33dec7aeefeeb565ce070c61a1d0a
Iftachts <167376409+Iftachts@users.noreply.github.com>
2024-04-18 10:06:00 +0300

    Update GeminiSpedweb.py

--- GeminiSpedweb.py
+++ GeminiSpedweb.py
@@ -17,22 +17,21 @@ generation_config = {
 safety_settings = [
   {
     "category": "HARM_CATEGORY_HARASSMENT",
-    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+    "threshold": "BLOCK_ONLY_HIGH"
   },
   {
     "category": "HARM_CATEGORY_HATE_SPEECH",
-    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+    "threshold": "BLOCK_ONLY_HIGH"
   },
   {
     "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+    "threshold": "BLOCK_ONLY_HIGH"
   },
   {
     "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
-    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+    "threshold": "BLOCK_ONLY_HIGH"
   },
 ]
-
 model = genai.GenerativeModel(model_name="gemini-1.5-pro-latest",
                               generation_config=generation_config,
                               safety_settings=safety_settings)
